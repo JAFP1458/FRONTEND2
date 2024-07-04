2f41a40782ce3197f34ec08cd0c4c54cd89f531f
jonathan faican <jafp1458@gmail.com>
2024-07-04 16:53:48 -0500

    Respaldo

--- src/layouts/profile/components/Header/index.js
+++ src/layouts/profile/components/Header/index.js
@@ -1,66 +1,60 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { useState, useEffect } from 'react';
-
-// prop-types is a library for typechecking of props.
 import PropTypes from 'prop-types';
-
-// @mui material components
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
 import AppBar from '@mui/material/AppBar';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Icon from '@mui/material/Icon';
-
-// Material Dashboard 2 React components
 import MDBox from 'components/MDBox';
 import MDTypography from 'components/MDTypography';
 import MDAvatar from 'components/MDAvatar';
-
-// Material Dashboard 2 React base styles
 import breakpoints from 'assets/theme/base/breakpoints';
-
-// Images
-import burceMars from 'assets/images/bruce-mars.jpg';
 import backgroundImage from 'assets/images/bg-profile.jpeg';
+import axios from 'axios';
+
+const defaultProfileImage = 'assets/images/team-1.jpg'; // Reemplaza con la URL de una imagen por defecto
 
 function Header({ children }) {
   const [tabsOrientation, setTabsOrientation] = useState('horizontal');
   const [tabValue, setTabValue] = useState(0);
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
-    // A function that sets the orientation state of the tabs.
     function handleTabsOrientation() {
       return window.innerWidth < breakpoints.values.sm
         ? setTabsOrientation('vertical')
         : setTabsOrientation('horizontal');
     }
 
-    /** 
-     The event listener that's calling the handleTabsOrientation function when resizing the window.
-    */
     window.addEventListener('resize', handleTabsOrientation);
-
-    // Call the handleTabsOrientation function to set the state with the initial value.
     handleTabsOrientation();
 
-    // Remove event listener on cleanup
     return () => window.removeEventListener('resize', handleTabsOrientation);
   }, [tabsOrientation]);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const usuarioID = localStorage.getItem('usuarioID');
+      const token = localStorage.getItem('token');
+      if (usuarioID && token) {
+        try {
+          const response = await axios.get(
+            `http://localhost:5000/users/details/${usuarioID}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          setUserData(response.data);
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+        }
+      }
+    };
+    fetchUserData();
+  }, []);
 
   const handleSetTabValue = (event, newValue) => setTabValue(newValue);
 
@@ -98,19 +92,20 @@ function Header({ children }) {
         <Grid container spacing={3} alignItems="center">
           <Grid item>
             <MDAvatar
-              src={burceMars}
+              src={userData.profileImage || defaultProfileImage}
               alt="profile-image"
               size="xl"
               shadow="sm"
+              sx={{ borderRadius: '50%', objectFit: 'cover' }}
             />
           </Grid>
           <Grid item>
             <MDBox height="100%" mt={0.5} lineHeight={1}>
               <MDTypography variant="h5" fontWeight="medium">
-                Richard Davis
+                {userData.nombre || 'N/A'}
               </MDTypography>
               <MDTypography variant="button" color="text" fontWeight="regular">
-                CEO / Co-Founder
+                {userData.correoelectronico || 'N/A'}
               </MDTypography>
             </MDBox>
           </Grid>
@@ -155,12 +150,10 @@ function Header({ children }) {
   );
 }
 
-// Setting default props for the Header
 Header.defaultProps = {
   children: '',
 };
 
-// Typechecking props for the Header
 Header.propTypes = {
   children: PropTypes.node,
 };
